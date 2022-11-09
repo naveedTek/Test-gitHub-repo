d25d0ab68dd99c2fe027bdb5f526fd80c16348f7
naveed <naveed.ahmad@tekschool.us>
2022-11-08 21:37:11 -0500

    pranch test

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" test for the new branch");
+		
+		
+		
+		
+
+	}
+
+}
